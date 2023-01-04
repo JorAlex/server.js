292e1597476fcb5793b417ad66afd1a46de1c450
JorAlex <alex.aries18@gmail.com>
2023-01-04 09:46:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola mundo');
+  res.end('Hello World');
 });
 
 server.listen(port, () => {
-  console.log(¨Server running on port: ${port}¨);
+  console.log(`Server running on port: ${port}`);
 });
